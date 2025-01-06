88da80f1a584a7f5dcd572fe76c8794dfc6016f5
GZhonghui <mail@gzher.com>
2025-01-06 16:48:29 +0900

    整理代码 & 更新README

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
 # taichi-mini
-帮助理解太极的工作原理
+
+太极（[Taichi](https://github.com/taichi-dev/taichi)）很方便，一个装饰器就可以将 Python 代码加速数十倍。
+
+但这是怎么做到的？
+
+通过翻阅太极的文档和代码，我实现了这个迷你版的太极，它也可以帮你加速代码。
+
+当然，它不支持GPU、不支持复杂的语法、没有数学库、加速效果也没有那么好。
+
+它不是为了用于实际的生产环境，而是尝试用 2k 行左右的代码，介绍一种编译 Python 代码的方式。
+
+## 运行
+```
+# 需要首先安装好 LLVM
+make
+python example.py
+
+# 在 Apple M1 芯片上的执行结果，大约加速了 60 倍
+[16:20:37] [MESSAGE] >> Taichi inited
+[16:20:37] [MESSAGE] >> function task_ti took 0.415144 seconds to execute
+[16:21:02] [MESSAGE] >> function task_no took 24.548944 seconds to execute
+[16:21:02] [MESSAGE] >> result check passed
+```
--- example.py
+++ example.py
@@ -17,21 +17,21 @@ def calc_ti(i: ti.Int64, magic: ti.Int64) -> ti.Int64:
 # 注意这两个修饰器的顺序 不能反过来
 @ti.log_time
 @ti.kernel
-def task_ti(n, data, offset = 3):
+def task_ti(n, data, magic = 3):
     for i in range(n):
-        data[i] = calc_ti(i, offset)
+        data[i] = calc_ti(i, magic)
 
 @ti.log_time
-def task_no(n, data, offset = 3):
+def task_no(n, data, magic = 3):
     for i in range(n):
-        data[i] = calc_ti.origin(i, offset)
+        data[i] = calc_ti.origin(i, magic)
 
 def main():
     N = int(4e4)
     data1, data2 = [0] * N, [0] * N
 
-    task_ti(N, data1, offset=5)
-    task_no(N, data2, offset=5)
+    task_ti(N, data1, magic=5)
+    task_no(N, data2, magic=5)
 
     ti.log_message(
         "result check passed"
